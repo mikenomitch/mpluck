e2b000ee21c67a1e5c144c27e0d1b722d15b0109
Michael Nomitch <mnomitch@instructure.com>
2015-05-16 14:57:24 -0500

    fixed issues

--- lib/mpluck.rb
+++ lib/mpluck.rb
@@ -1,17 +1,19 @@
 require "mpluck/version"
 
 ActiveRecord::Relation.class_eval do
-  def multipluck(cols_to_pluck)
-    connection.select_all( engine.select(cols_to_pluck) )
+  def multipluck(*cols_to_pluck)
+    connection.select_all( self.select(cols_to_pluck) )
   end
   alias_method :mpluck, :multipluck
 
   def grouped_multipluck(sorting_col, cols_to_pluck)
     cols_to_pluck << sorting_col unless cols_to_pluck.include?(sorting_col)
-    self.multipluck(cols_to_pluck).reduce({}) do |memo, record|
-      memo[record[sorting_col]] = record
+
+    self.multipluck(*cols_to_pluck).reduce({}) do |memo, record|
+      memo[record["#{sorting_col}"]] = record
       memo
     end
   end
+
   alias_method :grouped_mpluck, :grouped_multipluck
 end
--- lib/mpluck/version.rb
+++ lib/mpluck/version.rb
@@ -1,3 +1,3 @@
 module Mpluck
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
